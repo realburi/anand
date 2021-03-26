20497dc7d0ab974e09c43d87c54c07138c06c4d7
realburi <37531956+realburi@users.noreply.github.com>
2021-03-26 12:27:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
+import random
 
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
-# title icon
+background = pygame.image.load("2474215.jpg")
+
+#icon
 icon = pygame.image.load('outer-space-alien.png')
 pygame.display.set_icon(icon)
 pygame.display.set_caption("space invaders")
@@ -15,13 +18,28 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
+#enemy
+
+enemyIMG = pygame.image.load('ufo.png')
+enemyX = random.randint (0, 736)
+enemyY = random.randint (50, 150)
+enemyX_change = 0.3
+enemyY_change = 40
+
 def player(x,y):
     screen.blit(playerIMG, (playerX, playerY))
+
+def enemy(x, y):
+    screen.blit(enemyIMG, (enemyX, enemyY))
 
 #event
 running = True
 while running:
     screen.fill((255, 255, 255))
+
+    screen.blit(background, (0, 0))
+
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -38,10 +56,23 @@ while running:
         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
             playerX_change = 0
 
+    # player bounder
 
     playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    # enemy bounder
+    enemyX += enemyX_change
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
+
     player(playerX,playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
-
-
-
